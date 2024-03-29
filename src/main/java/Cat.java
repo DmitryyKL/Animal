be58cbdae89657a156ed592040eda7227d619f5e
Dmitry.KL <d.klokov.kl@gmail.com>
2024-03-29 23:58:34 +0500

    First save

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,4 +1,7 @@
 public class Cat extends Animal{
+    public Cat(){
+        super(false,"milk and meat");
+    }
     @Override
     public void sound() {
         System.out.println("meow");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Cat cat  = new Cat();
         cat.sound();
+        System.out.println(cat.getEats());
     }
 }
